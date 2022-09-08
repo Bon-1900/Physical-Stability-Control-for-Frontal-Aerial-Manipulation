451425151c84b5ef65f2618a99236d6cc5b32977
bon_1900 <bon_1900@yahoo.com>
2022-09-08 15:58:55 +0100

    plot physical testing data

--- Data/Physical Testing/exp1.1.1/exp1_1_1_plot.m
+++ Data/Physical Testing/exp1.1.1/exp1_1_1_plot.m
@@ -1,17 +1,21 @@
 clc;
 clear;
+close all;
 
 %% Process Data
-t_start = 0;
-t_end   = 80;
+t_start = 2;
+t_end   = 14.5;
 bag_paths = dir('*.bag');
 for i_bag_paths = 1:length(bag_paths)
-    [data_mat, sp_mat, err] = process_data (bag_paths(i_bag_paths).name, t_start, t_end);
+    [data_mat, sp_mat] = process_data (bag_paths(i_bag_paths).name, 0, 100);
+    [data_mat, sp_mat] = unify_start_t (data_mat, sp_mat);
+    data_mat = trim_data(t_start, t_end, data_mat);
+    sp_mat = trim_data(t_start, t_end, sp_mat);
     data_mats {i_bag_paths} = data_mat;
     sp_mats {i_bag_paths}   = sp_mat;
-    err_arrs {i_bag_paths}   = err;
+    err_arrs{i_bag_paths}   = get_err(sp_mats{i_bag_paths}(:,[2,3,4]), ...
+            data_mats{1,i_bag_paths}(:,[2,3,4]));
 end
-
 
 %% Plot xyz positions
 figure
@@ -20,6 +24,7 @@ for i_xyz=1:3
     subplot(3,1,i_xyz)
     hold on
     grid on
+    xlim([t_start,t_end])
     plot(sp_mats{1}(:,1), sp_mats{1}(:,i_xyz+1))
     for i_data = 1:length(data_mats)
         plot(data_mats{i_data}(:,1), data_mats{i_data}(:,(i_xyz+1)))
@@ -33,8 +38,22 @@ for i_xyz=1:3
         case 3
             ylabel('z position (m)')
     end
-    legend('Setpoint','baseline','1.0kg load','1.1kg load','1.2kg load','1.25kg load','1.3kg load','location','northwest')
 end
+legend('Setpoint', ...
+    '0.1kg load', ...
+    '0.2kg load', ...
+    '0.3kg load', ...
+    '0.4kg load', ...
+    '0.5kg load', ...
+    '0.6kg load', ...
+    '0.7kg load', ...
+    '0.8kg load', ...
+    '0.9kg load', ...
+    '1.0kg load', ...
+    '1.1kg load', ...
+    '1.2kg load',...
+    'baseline',...
+    'Position',[0.15,0.53,0.15,0.4])
 saveas(gcf,'Plots/exp1.1.1_xyz_plot.jpg')
 
 %% Plot transitional error
@@ -43,9 +62,24 @@ hold on
 grid on
 for i_data = 1:length(data_mats)
     plot(data_mats{i_data}(:,1), err_arrs{i_data})
+    xlim([t_start,t_end])
 end
 
-legend('baseline','1.0kg load','1.1kg load','1.2kg load','1.25kg load','1.3kg load','location','northwest')
+    legend( ...
+        '0.1kg load', ...
+        '0.2kg load', ...
+        '0.3kg load', ...
+        '0.4kg load', ...
+        '0.5kg load', ...
+        '0.6kg load', ...
+        '0.7kg load', ...
+        '0.8kg load', ...
+        '0.9kg load', ...
+        '1.0kg load', ...
+        '1.1kg load', ...
+        '1.2kg load', ...
+        'baseline',...
+        'Location','northwest')
 xlabel('Simulation Time (sec)')
 ylabel('Error (m)')
 title ('Exp1.1.1 Absolute Translational error')
@@ -53,7 +87,36 @@ saveas(gcf,'Plots/exp1.1.1_err_plot.jpg')
 
 
 %% HELPER FUNCS
-function [data_mat, sp_mat_remapped, err] = process_data (bag_path, start_t, end_t)
+function [data_mat, traj_mat] = unify_start_t (data_mat, traj_mat)
+    start_t_offset = 5;
+    old_start_time = data_mat(1,1);
+    start_pos_z = data_mat(1,4);
+    % get actual start time
+        j_stamp = 1;
+        while data_mat(j_stamp, 4) < start_pos_z+0.01
+            new_start_t = traj_mat(j_stamp, 1);
+            j_stamp = j_stamp +1;
+        end
+        new_start_t = new_start_t - start_t_offset;
+        end_index = length(data_mat);
+        end_t = traj_mat(end_index, 1);
+
+        % trim data based on new starting and ending time
+        data_mat = trim_data(new_start_t, end_t, data_mat);
+        data_mat(:,1) = data_mat(:,1) - new_start_t;
+
+        % trim traj mat
+        end_index = length(data_mat);
+        end_t = traj_mat(end_index, 1);
+        traj_mat = trim_data(old_start_time, end_t, traj_mat);
+
+        traj_mat = remap_sp (traj_mat(:,[2,3,4]), traj_mat(:,1), data_mat(:,1));
+ 
+
+
+end
+
+function [data_mat, sp_mat_remapped] = process_data (bag_path, start_t, end_t)
 % extract from ros bag
 bag = rosbag(bag_path);
 estimated_odom_topic = select(bag,'Topic','/neptune/neptune/vrpn_client/estimated_odometry');
@@ -63,7 +126,7 @@ traj_messages = readMessages(traj_reference_topic,'DataFormat','struct');
 odom_messages = readMessages(estimated_odom_topic,'DataFormat','struct');
 
 for i = 1:length(traj_messages)
-    traj{i,1} = (double(traj_messages{i,1}.Header.Seq) - double(traj_messages{1,1}.Header.Seq)) * 0.1;
+    traj{i,1} = (double(traj_messages{i,1}.Header.Seq)-1) * 0.1;
     traj{i,2} = traj_messages{i,1}.Pose.Position.X;
     traj{i,3} = traj_messages{i,1}.Pose.Position.Y;
     traj{i,4} = traj_messages{i,1}.Pose.Position.Z;
@@ -75,7 +138,7 @@ for i = 1:length(odom_messages)
     odom{i,1} = double(odom_messages{i,1}.Header.Stamp.Sec) ...
               + double(odom_messages{i,1}.Header.Stamp.Nsec) * 1e-9...
               - double(odom_messages{1,1}.Header.Stamp.Sec) ...
-              - double(odom_messages{1,1}.Header.Stamp.Nsec) * 1e-9;
+              - double(odom_messages{1,1}.Header.Stamp.Nsec) * 1e-9; 
     odom{i,2} = odom_messages{i,1}.Pose.Pose.Position.X;
     odom{i,3} = odom_messages{i,1}.Pose.Pose.Position.Y;
     odom{i,4} = odom_messages{i,1}.Pose.Pose.Position.Z;
@@ -87,13 +150,14 @@ data_mat = cellfun(@(od) double(od), odom);
 data_mat(isnan(data_mat)) =0;
 sp_mat(isnan(sp_mat))     =0;
 
-% trim data
-data_mat = trim_data (start_t, end_t, data_mat);
-sp_mat   = trim_data (start_t, end_t, sp_mat);
-
 % remap setpoint data timestamp to match data timestamps
 sp_mat_remapped = remap_sp (sp_mat(:,[2,3,4]), sp_mat(:,1), data_mat(:,1));
-err             = get_err (sp_mat_remapped(:,[2,3,4]), data_mat(:,[2,3,4]));
+
+% trim data
+data_mat = trim_data (start_t, end_t, data_mat);
+sp_mat_remapped   = trim_data (start_t, end_t, sp_mat_remapped);
+
+
 end
 
 function trimmed_mat = trim_data (start_t, end_t, mat)
--- Data/Physical Testing/exp1.2.5/exp1_2_5_error_plot.m
+++ Data/Physical Testing/exp1.2.5/exp1_2_5_error_plot.m
@@ -1,138 +1,96 @@
 clc;
 clear;
+close all;
 
 %% Process Data
-t_start = 0;
-t_end   = 80;
+t_start = 2;
+t_end   = 65;
 bag_paths = dir('*.bag');
 for i_bag_paths = 1:length(bag_paths)
-    [data_mat, sp_mat, err] = process_data (bag_paths(i_bag_paths).name, t_start, t_end);
+    [data_mat, sp_mat] = process_data (bag_paths(i_bag_paths).name, 0, 100);
+    [data_mat, sp_mat] = unify_start_t (data_mat, sp_mat);
+    data_mat = trim_data(t_start, t_end, data_mat);
+    sp_mat = trim_data(t_start, t_end, sp_mat);
     data_mats {i_bag_paths} = data_mat;
     sp_mats {i_bag_paths}   = sp_mat;
-    err_arrs {i_bag_paths}   = err;
+    err_arrs{i_bag_paths}   = get_err(sp_mats{i_bag_paths}(:,[2,3,4]), ...
+            data_mats{1,i_bag_paths}(:,[2,3,4]));
 end
-
 
 %% Plot xyz positions
-f_xyz = figure;
-f_xyz.Position = [240  12  1134  849];
+figure
 sgtitle('Exp1.2.5 Position Data')
-
-for i_plot=1:6
-    for i_xyz=1:3
-        subplot(6,3,(i_plot-1)*3+i_xyz)
-        hold on
-        grid on
-        plot(sp_mats{1}(:,1), sp_mats{1}(:,i_xyz+1))
-        plot(data_mats{1}(:,1), data_mats{1}(:,(i_xyz+1)))
-
-        switch i_plot
-            case 1
-                plot(data_mat_rw0_00_fw0_225(:,1), data_mat_rw0_00_fw0_225(:,(i_xyz+1)));
-                plot(data_mat_rw0_29_fw0_66(:,1), data_mat_rw0_29_fw0_66(:,(i_xyz+1)))
-            case 2
-                plot(data_mat_rw0_05_fw0_25(:,1), data_mat_rw0_05_fw0_25(:,(i_xyz+1)))
-                plot(data_mat_rw0_05_fw0_30(:,1), data_mat_rw0_05_fw0_30(:,(i_xyz+1)))
-            case 3
-                plot(data_mat_rw0_10_fw0_30(:,1), data_mat_rw0_10_fw0_30(:,(i_xyz+1)))
-                plot(data_mat_rw0_10_fw0_35(:,1), data_mat_rw0_10_fw0_35(:,(i_xyz+1)))
-            case 4
-                plot(data_mat_rw0_15_fw0_40(:,1), data_mat_rw0_15_fw0_40(:,(i_xyz+1)))
-                plot(data_mat_rw0_15_fw0_45(:,1), data_mat_rw0_15_fw0_45(:,(i_xyz+1)))
-            case 5
-                plot(data_mat_rw0_20_fw0_45(:,1), data_mat_rw0_20_fw0_45(:,(i_xyz+1)))
-                plot(data_mat_rw0_20_fw0_50(:,1), data_mat_rw0_20_fw0_50(:,(i_xyz+1)))
-            case 6
-                plot(data_mat_rw0_25_fw0_55(:,1), data_mat_rw0_25_fw0_55(:,(i_xyz+1)))
-                plot(data_mat_rw0_25_fw0_60(:,1), data_mat_rw0_25_fw0_60(:,(i_xyz+1)))
-        end
-
-        xlabel('Simulation Time (sec)')
-        switch i_xyz
-            case 1
-                ylabel('x position (m)')
-            case 2
-                ylabel('y position (m)')
-            case 3
-                ylabel('z position (m)')
-        end
-        xlim([t_start, t_end])
-    end
-    switch i_plot
-        case 1
-            lgd1 = legend('Setpoint','baseline', 'RW0.00 FW0.225','RW0.29 FW0.66', ...
-                'Position',[0.3493    0.9125    0.3250    0.0177], 'Orientation','horizontal');
-        case 2
-            lgd2 = legend('Setpoint','baseline', 'RW0.05 FW0.25', 'RW0.05 FW0.30',  ...
-                'Position',[0.3493    0.7724     0.3250    0.0177], 'Orientation','horizontal');
-        case 3
-            lgd3 = legend('Setpoint','baseline', 'RW0.10 FW0.30', 'RW0.10 FW0.35',  ...
-                'Position',[0.3493    0.6310    0.3250    0.0177], 'Orientation','horizontal');
-        case 4
-            lgd4 = legend('Setpoint','baseline', 'RW0.15 FW0.40', 'RW0.15 FW0.45',  ...
-                'Position',[0.3493    0.4885    0.3250    0.0177], 'Orientation','horizontal');
-        case 5
-            lgd5 = legend('Setpoint','baseline', 'RW0.20 FW0.45', 'RW0.20 FW0.50',  ...
-                'Position',[0.3493    0.3495    0.3250    0.0177], 'Orientation','horizontal');
-        case 6
-            lgd6 = legend('Setpoint','baseline', 'RW0.25 FW0.55', 'RW0.25 FW0.60',  ...
-                'Position',[0.3493    0.2070    0.3250    0.0177], 'Orientation','horizontal');
-    end
-
-end
-
-saveas(gcf,'Plots/exp1.2.5_xyz_plot.jpg')
-
-
-%% Plot transitional error
-f_err = figure;
-f_err.Position = [468   169   892   692];
-sgtitle ('Exp1.2.5 Absolute Translational error')
-for i_plot=1:6
-    subplot(3,2,i_plot)
+for i_xyz=1:3
+    subplot(3,1,i_xyz)
     hold on
     grid on
-
-    plot(data_mats{1}(:,1), err_arrs{1})
-
-    switch i_plot
-        case 1
-            plot(data_mat_rw0_00_fw0_225(:,1), err_rw0_00_fw0_225)
-            plot(data_mat_rw0_29_fw0_66(:,1), err_rw0_29_fw0_66)
-            legend('baseline', 'RW0.00 FW0.225', 'RW0.29 FW0.66', 'Location','best')
-        case 2
-            plot(data_mat_rw0_05_fw0_25(:,1), err_rw0_05_fw0_25)
-            plot(data_mat_rw0_05_fw0_30(:,1), err_rw0_05_fw0_30)
-            legend( 'baseline', 'RW0.05 FW0.25', 'RW0.05 FW0.30', 'Location','best')
-        case 3
-            plot(data_mat_rw0_10_fw0_30(:,1), err_rw0_10_fw0_30)
-            plot(data_mat_rw0_10_fw0_35(:,1), err_rw0_10_fw0_35)
-            legend( 'baseline', 'RW0.10 FW0.30', 'RW0.10 FW0.35', 'Location','best')
-        case 4
-            plot(data_mat_rw0_15_fw0_40(:,1), err_rw0_15_fw0_40)
-            plot(data_mat_rw0_15_fw0_45(:,1), err_rw0_15_fw0_45)
-            legend( 'baseline', 'RW0.15 FW0.40', 'RW0.15 FW0.45', 'Location','best')
-        case 5
-            plot(data_mat_rw0_20_fw0_45(:,1), err_rw0_20_fw0_45)
-            plot(data_mat_rw0_20_fw0_50(:,1), err_rw0_20_fw0_50)
-            legend( 'baseline', 'RW0.20 FW0.45', 'RW0.20 FW0.50', 'Location','best')
-        case 6
-            plot(data_mat_rw0_25_fw0_55(:,1), err_rw0_25_fw0_55)
-            plot(data_mat_rw0_25_fw0_60(:,1), err_rw0_25_fw0_60)
-            legend( 'baseline','RW0.25 FW0.55', 'RW0.25 FW0.60', 'Location','best')
+    xlim([t_start,t_end])
+    plot(sp_mats{1}(:,1), sp_mats{1}(:,i_xyz+1))
+    for i_data = 1:length(data_mats)
+        plot(data_mats{i_data}(:,1), data_mats{i_data}(:,(i_xyz+1)))
     end
     xlabel('Simulation Time (sec)')
-    ylabel('Error (m)')
-    xlim([t_start, t_end])
+    switch i_xyz
+        case 1
+            ylabel('x position (m)')
+        case 2
+            ylabel('y position (m)')
+        case 3
+            ylabel('z position (m)')
+    end
 end
+legend('Setpoint', ...
+    'Baseline',...
+   'Position',[0.8,0.87,0.15,0.063])
+saveas(gcf,'Plots/exp1.2.5_xyz_plot.jpg')
 
+%% Plot transitional error
+figure
+hold on
+grid on
+for i_data = 1:length(data_mats)
+    plot(data_mats{i_data}(:,1), err_arrs{i_data})
+    xlim([t_start,t_end])
+end
+    legend( 'baseline',...
+        'Location','northwest')
+xlabel('Simulation Time (sec)')
+ylabel('Error (m)')
+title ('Exp1.2.5 Absolute Translational error')
 saveas(gcf,'Plots/exp1.2.5_err_plot.jpg')
 
 
-
-
 %% HELPER FUNCS
-function [data_mat, sp_mat_remapped, err] = process_data (bag_path, start_t, end_t)
+function [data_mat, traj_mat] = unify_start_t (data_mat, traj_mat)
+    start_t_offset = 5;
+    old_start_time = data_mat(1,1);
+    start_pos_z = data_mat(1,4);
+    % get actual start time
+        j_stamp = 1;
+        while data_mat(j_stamp, 4) < start_pos_z+0.01
+            new_start_t = traj_mat(j_stamp, 1);
+            j_stamp = j_stamp +1;
+        end
+        new_start_t = new_start_t - start_t_offset;
+        end_index = length(data_mat);
+        end_t = traj_mat(end_index, 1);
+
+        % trim data based on new starting and ending time
+        data_mat = trim_data(new_start_t, end_t, data_mat);
+        data_mat(:,1) = data_mat(:,1) - new_start_t;
+
+        % trim traj mat
+        end_index = length(data_mat);
+        end_t = traj_mat(end_index, 1);
+        traj_mat = trim_data(old_start_time, end_t, traj_mat);
+
+        traj_mat = remap_sp (traj_mat(:,[2,3,4]), traj_mat(:,1), data_mat(:,1));
+ 
+
+
+end
+
+function [data_mat, sp_mat_remapped] = process_data (bag_path, start_t, end_t)
 % extract from ros bag
 bag = rosbag(bag_path);
 estimated_odom_topic = select(bag,'Topic','/neptune/neptune/vrpn_client/estimated_odometry');
@@ -142,17 +100,19 @@ traj_messages = readMessages(traj_reference_topic,'DataFormat','struct');
 odom_messages = readMessages(estimated_odom_topic,'DataFormat','struct');
 
 for i = 1:length(traj_messages)
-    traj{i,1} = (double(traj_messages{i,1}.Header.Seq) - double(traj_messages{1,1}.Header.Seq)) * 0.1;
+    traj{i,1} = (double(traj_messages{i,1}.Header.Seq)-1) * 0.1;
     traj{i,2} = traj_messages{i,1}.Pose.Position.X;
     traj{i,3} = traj_messages{i,1}.Pose.Position.Y;
     traj{i,4} = traj_messages{i,1}.Pose.Position.Z;
-
 end
 
 sp_mat = cellfun(@(tr) double(tr), traj);
 
 for i = 1:length(odom_messages)
-    odom{i,1} = 1000 * (odom_messages{i,1}.Header.Stamp.Sec - traj_reference_topic.StartTime);
+    odom{i,1} = double(odom_messages{i,1}.Header.Stamp.Sec) ...
+              + double(odom_messages{i,1}.Header.Stamp.Nsec) * 1e-9...
+              - double(odom_messages{1,1}.Header.Stamp.Sec) ...
+              - double(odom_messages{1,1}.Header.Stamp.Nsec) * 1e-9; 
     odom{i,2} = odom_messages{i,1}.Pose.Pose.Position.X;
     odom{i,3} = odom_messages{i,1}.Pose.Pose.Position.Y;
     odom{i,4} = odom_messages{i,1}.Pose.Pose.Position.Z;
@@ -164,13 +124,14 @@ data_mat = cellfun(@(od) double(od), odom);
 data_mat(isnan(data_mat)) =0;
 sp_mat(isnan(sp_mat))     =0;
 
-% trim data
-data_mat = trim_data (start_t, end_t, data_mat);
-sp_mat   = trim_data (start_t, end_t, sp_mat);
-
 % remap setpoint data timestamp to match data timestamps
 sp_mat_remapped = remap_sp (sp_mat(:,[2,3,4]), sp_mat(:,1), data_mat(:,1));
-err             = get_err (sp_mat_remapped(:,[2,3,4]), data_mat(:,[2,3,4]));
+
+% trim data
+data_mat = trim_data (start_t, end_t, data_mat);
+sp_mat_remapped   = trim_data (start_t, end_t, sp_mat_remapped);
+
+
 end
 
 function trimmed_mat = trim_data (start_t, end_t, mat)
